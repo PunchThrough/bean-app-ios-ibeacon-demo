3528032534b121ee26a3d427f22992c649f05a79
Matthew Lewis <matt@mplewis.com>
2015-12-18 16:09:16 -0600

    Initial Commit

---
+++ Bean iB Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bean iB Demo
+//
+//  Created by Matthew Lewis on 12/18/15.
+//  Copyright © 2015 Punch Through Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
